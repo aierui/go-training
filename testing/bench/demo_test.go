e7e8514028d89845fe41f3c8b317791121f24e71
aierui <aieruishi@gmail.com>
2020-06-14 23:59:46 +0800

    add testings package

---
+++ testing/bench/demo_test.go
@@ -0,0 +1,28 @@
+package bench
+
+import "testing"
+
+func BenchmarkGetPrimes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetPrimes(1000)
+	}
+}
+
+func BenchmarkGetPrimesWith100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetPrimes(100)
+	}
+}
+
+func BenchmarkGetPrimesWith10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetPrimes(10000)
+	}
+}
+
+func BenchmarkGetPrimesWith1000000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetPrimes(1000000)
+	}
+}
+
