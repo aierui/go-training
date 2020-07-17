d3ff3d2707bc9dedd2f2e673b340699d351f808e
aierui <aieruishi@gmail.com>
2020-07-17 23:37:55 +0800

    format code

--- language-specification/map_Immutability.go
+++ language-specification/map_Immutability.go
@@ -1,22 +1,16 @@
 package main
 
-
-
-package main
-
 type S struct {
 	name string
 }
 
 // Cannot assign to m["x"].name
 func main() {
-	m := map[string]S{"x": S{"one"}}
+	m := map[string]S{"x": {"one"}}
 	m["x"].name = "two"
 }
 
-
-
 func main2() {
-	m := map[string]*S{"x": &S{"one"}}
+	m := map[string]*S{"x": {"one"}}
 	m["x"].name = "two"
 }
