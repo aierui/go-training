2cf108c9e57110e45875f10b0b9796a6fb3978a3
aierui <aieruishi@gmail.com>
2020-11-26 00:09:43 +0800

    update goroutine declare

--- goroutine/declare/main.go
+++ goroutine/declare/main.go
@@ -8,4 +8,24 @@ func main() {
 			fmt.Println(i)
 		}()
 	}
+
+	/*
+		➜  declare git:(master) ✗ go vet main.go
+		# command-line-arguments
+		./main.go:8:16: loop variable i captured by func literal
+	*/
+	// 解法1
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
+	}
+
+	// 解法2
+	for i := 0; i < 10; i++ {
+		i := i
+		go func() {
+			fmt.Println(i)
+		}()
+	}
 }
