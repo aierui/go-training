2ea1c93023103e8c596079891e496cdee7dd1027
aierui <aieruishi@gmail.com>
2020-12-17 00:23:38 +0800

    c

--- map/example/main.go
+++ map/example/main.go
@@ -30,6 +30,29 @@ func main() {
 		<nil>
 		[1 2]
 	*/
+
+	m2 := make(map[string]int, 0)
+	m2V := m2["k2"]
+	fmt.Printf("m2V %T->%v\n", m2V, m2V)
+
+	m3 := make(map[string]string, 0)
+	m3V := m3["k3"]
+	fmt.Printf("m3V %T->%v\n", m3V, m3V)
+
+	m4 := make(map[string]interface{}, 0)
+	m4V := m4["k4"]
+	fmt.Printf("m4V %T->%v\n", m4V, m4V)
+
+	m5 := make(map[string]float64, 0)
+	m5V := m5["k5"]
+	fmt.Printf("m5V %T->%v\n", m5V, m5V)
+
+	/*
+		m2V int->0
+		m3V string->
+		m4V <nil>-><nil>
+		m5V float64->0
+	*/
 }
 
 func setValue(m map[string]interface{}) error {
