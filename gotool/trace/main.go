ec85dbeaf9bb56148b8b3c5c8405c9ff737768ef
aierui <aieruishi@gmail.com>
2021-04-27 00:43:32 +0800

    c

--- gotool/trace/main.go
+++ gotool/trace/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
-	"os"
-	"runtime/trace"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
-	trace.Start(os.Stderr)
-	defer trace.Stop()
 
-	ch := make(chan string)
-	go func() {
-		ch <- "like"
-	}()
+	_ = http.ListenAndServe(":7777", nil)
 
-	<-ch
+	m()
 
-	//step1. go run main.go 2> trace.out
+	// web visual view （直接用浏览器查看 简直太方便了）
+	// http://127.0.0.1:7777/debug/pprof/
+
+	// https://golang.org/doc/diagnostics#profiling
+	// trace
+	//step1. go run main.go
+	//step2. curl 'http://127.0.0.1:7777/debug/pprof/trace?seconds=20' > trace.out
 	//step2. go tool trace trace.out
+
+	// pprof
+	//step1. go run main
+	//step2. curl 'http://127.0.0.1:7777/debug/pprof/profile?seconds=20' > profile.out
+	//step3. go tool pprof -http=:9090  profile.out
+}
+
+func m() {
+	rows := make([]string, 10)
+
+	for i := 0; i < 10; i++ {
+		rows = append(rows, "111")
+	}
+
+	for {
+		rows = append(rows, "111")
+	}
 }
